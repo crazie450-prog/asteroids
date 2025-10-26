022bdca1009c222058e10cd94b922543c11aa485
crazie450-prog <crazie450@gmail.com>
2025-10-26 15:38:02 -0700

    added shooting functionality for the player ship.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from circleshape import CircleShape
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main():
@@ -20,9 +21,11 @@ def main():
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = updateable, drawable
     Asteroid.containers = updateable, drawable, asteroids
     AsteroidField.containers = updateable
+    Shot.containers = updateable, drawable, shots
     
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
@@ -37,6 +40,11 @@ def main():
         
         screen.fill((0, 0, 0))  # Clear screen with black
         updateable.update(dt)
+        
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game Over!")
+                return
         for entity in drawable:
                 entity.draw(screen)
             
--- player.py
+++ player.py
@@ -1,12 +1,15 @@
 from circleshape import CircleShape
 import pygame
 from constants import *
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.velocity = pygame.Vector2(0, 0)
         self.rotation = 0  # in degrees
+        self.shoot_cooldown = 0.3  # seconds
+        self.shoot_timer = 0.0
         
     # in the player class
     def triangle(self):
@@ -29,6 +32,7 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
         
     def update(self, dt):
+        self.shoot_timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -39,3 +43,15 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+            
+    def shoot(self):
+        if self.shoot_timer <= 0.0:
+            self.shoot_timer = self.shoot_cooldown
+            shot = Shot(self.position.x, self.position.y)
+            forward = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+            shot.velocity = forward
+            return shot
+        else:
+            return None
