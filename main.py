09ef532b374bd3263fb87945d2ffb2b4d02e7434
crazie450-prog <crazie450@gmail.com>
2025-10-26 11:40:43 -0700

    added pygame box

--- main.py
+++ main.py
@@ -2,10 +2,21 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
     print("Starting Asteroids!")
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)   
 
 
 if __name__ == "__main__":
